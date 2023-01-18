c9e4e70a7f4cf5e3b7d8079a577a490335b93182
Vigneshkrishnan0001 <110534882+Vigneshkrishnan0001@users.noreply.github.com>
2023-01-18 16:52:30 +0530

    Update github.groovy

--- vars/github.groovy
+++ vars/github.groovy
@@ -1,5 +1,5 @@
 def call(repo){
-git branch: main, changelog: false, credentialsId: false, poll: false, url: "git@github.com:Vigneshkrishnan0001/jenkins.git"
+git branch: main, changelog: false, credentialsId: false, poll: false, url: "git@github.com:Vigneshkrishnan0001/$repo.git"
 }
 
 return this
